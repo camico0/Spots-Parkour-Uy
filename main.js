de545d45c66a90fb054cbe645a671c0ebbbac971
camico0 <camilacor95@gmail.com>
2022-09-07 21:41:00 -0300

    Carousel finished

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-const productContainers = [...document.querySelectorAll(".product-container")];
+// window.addEventListener("load", iniciar, false);
+
+// function iniciar() {
+//     let bars_menu = document.getElementById("bars_menu");
+//     bars_menu.addEventListener("mouseover", menuJump);
+// }
+
+// function menuJump() {
+//     let bars_menu = (document.getElementById("bars_menu").innerHTML =
+//         "img/bars-2");
+// }
+
+const productContainers = [...document.querySelectorAll(".spot-container")];
 const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
 const preBtn = [...document.querySelectorAll(".pre-btn")];
 
